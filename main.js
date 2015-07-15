8f3cc9843a3313febbb55544e6d1bee17dbc43fb
nmonti <nmonti@umass.edu>
2015-07-15 18:54:06 -0400

    directory shizz

--- main.js
+++ main.js
@@ -21,27 +21,27 @@ function simulate(){
 function jam(groove){
   switch(groove) {
     case 0:
-      $('#audience').attr('src', 'gig-simulator/audio/begin.mp3');
+      $('#audience').attr('src', 'audio/begin.mp3');
       $('#audience')[0].play();
       break;
     case 1:
-      $('#audience').attr('src', 'gig-simulator/audio/booglass.mp3');
+      $('#audience').attr('src', 'audio/booglass.mp3');
       $('#audience')[0].play();
       break;
     case 2:
-      $('#audience').attr('src', 'gig-simulator/audio/getoff.mp3');
+      $('#audience').attr('src', 'audio/getoff.mp3');
       $('#audience')[0].play();
       break;
     case 3:
-      $('#audience').attr('src', 'gig-simulator/audio/getonwithit.mp3');
+      $('#audience').attr('src', 'audio/getonwithit.mp3');
       $('#audience')[0].play();
       break;
     case 4:
-      $('#audience').attr('src', 'gig-simulator/audio/kickapoo.mp3');
+      $('#audience').attr('src', 'audio/kickapoo.mp3');
       $('#audience')[0].play();
       break;
     case 5:
-      $('#audience').attr('src', 'gig-simulator/audio/singsomething.mp3');
+      $('#audience').attr('src', 'audio/singsomething.mp3');
       $('#audience')[0].play();
       break;
 
