88fc2c70300ee1d1068e2fb5c60312d4c7871c1a
nmonti <nmonti@umass.edu>
2015-07-23 22:05:27 -0400

    preload sounds; closes #1

--- main.js
+++ main.js
@@ -1,51 +1,65 @@
-$(document).ready(function() {
-  
+$(window).load(function() {
+  musterHeckles(insultCount)
+
+  // Start simulator
   $('#btn').click(function() {
-    heckle(0);
-    $(this).fadeOut(1500, function(){
+    $('#begin')[0].play();
+    $(this).fadeOut(300);
+    window.setTimeout(function(){
+      simulate();
       $('#background').css('opacity', 1);
-    });
+    }, 1700);
   });
 
-  simulate();
+  // Click to pause
+  $('#background').click(function(){
+    // make sure it's not null. will only happens before first insult
+    if (currentInsult){
+      if (paused){
+        paused = false;
+        $('#pause').hide();
+        currentInsult.play();
+        simulate();
+      }
+      else{
+        paused = true;
+        $('#pause').show();
+        currentInsult.pause();
+        clearInterval(interval);
+      }
+    }
+  });
 
 });
 
+var paused = false;
+var interval;
+var currentInsult;
+var insults = ['audio/begin.mp3',//..........0
+               'audio/booglass.mp3',//.......1 
+               'audio/getoff.mp3',//.........2
+               'audio/getonwithit.mp3',//....3
+               'audio/kickapoo.mp3',//.......4
+               'audio/singsomething.mp3'];//.5
+
+var insultCount = insults.length;
+
+// plays an insult every 5 seconds
 function simulate(){
-  setInterval(function(){
-    var insult = Math.floor(Math.random() * 5) + 1;
-    heckle(insult);
-  }, 4000);
+  interval = setInterval(function(){ heckle() }, 5000);
 }
 
-function heckle(insult){
-  switch(insult) {
-    case 0:
-      $('#audience').attr('src', 'audio/begin.mp3');
-      $('#audience')[0].play();
-      break;
-    case 1:
-      $('#audience').attr('src', 'audio/booglass.mp3');
-      $('#audience')[0].play();
-      break;
-    case 2:
-      $('#audience').attr('src', 'audio/getoff.mp3');
-      $('#audience')[0].play();
-      break;
-    case 3:
-      $('#audience').attr('src', 'audio/getonwithit.mp3');
-      $('#audience')[0].play();
-      break;
-    case 4:
-      $('#audience').attr('src', 'audio/kickapoo.mp3');
-      $('#audience')[0].play();
-      break;
-    case 5:
-      $('#audience').attr('src', 'audio/singsomething.mp3');
-      $('#audience')[0].play();
-      break;
-
+// Creates an audio element for each insult
+function musterHeckles(insultCount){
+  // Beginning audio loaded first
+  $('#audio').append("<audio preload='auto' src='"+insults[0]+"' id='begin'></audio");
+  for (var i = 1; i < insultCount; i++){
+    $('#audio').append("<audio preload='auto' src='"+insults[i]+"' id='insult"+i+"'></audio");
   }
-  
 }
 
+function heckle(){
+  var i = Math.floor(Math.random() * 5) + 1;
+  currentInsult = $('#insult'+i)[0];
+  currentInsult.play();
+}
